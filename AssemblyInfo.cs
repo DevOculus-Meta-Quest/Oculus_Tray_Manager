bf25ab936b12c16f39e522ebe8c597a7c63b4bee
Eliminater74 <eliminater74@gmail.com>
2025-12-14 15:21:01 -0500

    feat: Add the main application form (`FrmMain.cs`) and update project configuration and assembly information.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,12 +2,12 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Meta Quest Tray Tool")]
-[assembly: AssemblyDescription("https://Eliminater74.wixsite.com/vrtools")]
+[assembly: AssemblyDescription("https://github.com/DevOculus-Meta-Quest/Oculus_Tray_Manager")]
 [assembly: AssemblyCompany("Eliminater74")]
 [assembly: AssemblyProduct("Meta Quest Tray Tool")]
-[assembly: AssemblyCopyright("Copyright Eliminater74 ©  2021")]
+[assembly: AssemblyCopyright("Copyright Eliminater74 ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b56dbadf-9299-4657-8c10-f466449659f3")]
-[assembly: AssemblyFileVersion("0.87.8.0")]
-[assembly: AssemblyVersion("0.87.8.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
